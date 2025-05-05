6410d4985ce176ad665f30f7f704e74fed6b4484
Aleksandar <aleksandar@e-vlasotince.info>
2025-05-05 10:54:24 +0200

    homepage

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,12 @@
 import { Stack } from "expo-router";
+
+import { StatusBar } from "expo-status-bar";
 import "./globals.css";
 
 export default function RootLayout() {
   return (
     <>
+      <StatusBar style="light" />
       <Stack>
         <Stack.Screen options={{ headerShown: false }} name="(tabs)" />
         <Stack.Screen options={{ headerShown: false }} name="movies/[id]" />
